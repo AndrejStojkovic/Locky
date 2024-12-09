6ba10135db82631bdb4fd2b7203a133579419ba3
kalcovski <martin@iwmnetwork.com>
2024-12-09 18:11:02 +0100

    fix image preview issues

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -31,21 +31,9 @@ def get_project_path():
     return base_path
 
 def get_resource_path(filename):
-    """
-        Get the absolute path to a file, works for both .py and .exe files.
-
-        Args:
-            relative_path (str): The relative path to the file.
-
-        Returns:
-            str: The absolute path to the file.
-        """
-    if getattr(sys, 'frozen', False):
-        # If running in a PyInstaller bundle (exe), use sys._MEIPASS
-        base_path = sys._MEIPASS
-    else:
-        # If running as a script, use the directory of the main script
-        base_path = os.path.dirname(os.path.abspath(__file__))  # Use __file__ for proper reference
-
-    absolute_path = os.path.join(base_path, filename)
-    return absolute_path
+    """Returns the path to the resource file relative to the project directory."""
+    project_path = get_project_path()
+    resource_path = os.path.join(project_path, filename)
+    if not os.path.isfile(resource_path):
+        print(f"Error: Resource file not found at {resource_path}")
+    return resource_path
